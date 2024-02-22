713d607d94671f18d2b03105db49679c97bc20bc
mamunur1296 <mamunurrushid60@gmail.com>
2024-02-22 10:00:06 +0000

    Users Area Thim Satap SuccessFully 22 feb 2024

---
+++ Tutor Management System/Areas/Users/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tutor_Management_System.Areas.Users.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Courses()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+    }
+}
--- Tutor Management System/Program.cs
+++ Tutor Management System/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{area=Users}/{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
